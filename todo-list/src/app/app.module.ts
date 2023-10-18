c371984a89e1a142f673404c531e7f8211787bf7
Alerrando <alerrando2@gmail.com>
2023-10-18 16:25:12 -0300

    feat: changing layout

--- todo-list/src/app/app.module.ts
+++ todo-list/src/app/app.module.ts
@@ -4,23 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TasksComponent } from './tasks/tasks.component';
-import { TaskCreateComponent } from './task-create/task-create.component';
 import { Routes, RouterModule } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common'
+
 
 const routes:Routes = [
   { path: "", component: TasksComponent },
-  { path: "task-create", component: TaskCreateComponent }
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
     TasksComponent,
-    TaskCreateComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgOptimizedImage,
     RouterModule.forRoot(routes)
   ],
   providers: [RouterModule],
